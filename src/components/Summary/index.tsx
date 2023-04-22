ce3d0b77a21a6483c237356d03de7b056b1c40c4
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-04-22 19:09:24 -0300

    feat: add datetime and price formatter

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,6 +6,7 @@ import {
 import { SummaryCard, SummaryContainer } from './styles'
 import { useContext } from 'react'
 import { TransactionsContext } from '../../contexts/TransactionContext'
+import { priceFormatter } from '../../utils/formatter'
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext)
@@ -39,7 +40,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b373" />
         </header>
 
-        <strong>R$ {summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -48,7 +49,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$ {summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant={'green'}>
@@ -57,7 +58,7 @@ export function Summary() {
           <CurrencyCircleDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ {summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
